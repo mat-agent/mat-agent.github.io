260ee42d01b8e3120a7666a36c4233646db5937f
Pengxiang-Li <pengxiangli1999@gmail.com>
2024-12-30 10:10:30 +0800

    github

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -96,7 +96,7 @@
         </a>
       </span>
       <span class="link-block">
-        <a href="https://github.com/BIGAI-ML/TongAgent" class="external-link button is-normal is-rounded is-dark">
+        <a href="https://github.com/BIGAI-ML/TongAgent/tree/release" class="external-link button is-normal is-rounded is-dark">
           <span class="icon">
             <i class="fab fa-github"></i>
           </span>
